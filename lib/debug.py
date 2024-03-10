56390f038d7173ea89599a19dd158d68389c9e47
Rebekah Ndanu <rebekah.ndanu@student.moringaschool.com>
2024-03-10 22:20:50 +0300

    Completed core deliverables

--- lib/debug.py
+++ lib/debug.py
@@ -7,5 +7,7 @@ from classes.many_to_many import Result
 
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
-
+    # game1 = Game("Soduku")
+    # player1 = Player("Ndanu")
+    # result1 = Result(player1, game1, 10)
     ipdb.set_trace()
